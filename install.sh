e8c33ec2133deecf296e678fd1ee280f8268aaf0
rifqikhanif <94571775+rifqikhanif@users.noreply.github.com>
2022-04-27 07:19:10 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc	
+nvm install 16.14.2
+sudo apt install ffmpeg -y
+sudo apt install webp -y
+sudo apt install imagemagick -y
+node -v
+npm -v
